63ef86e04dfbaa8293a95591a3fcdd57a67907d5
TotalHelix <reubencoll@gmail.com>
2024-07-14 21:50:08 -0400

    swash position collection is complete, overlays might be harder than expected

--- calibrators.py
+++ calibrators.py
@@ -1,12 +1,60 @@
-class Calibrator():
+from pynput import mouse
+import overlay_lib as overlay
+
+
+class Calibrator:
     def __init__(self):
-        pass
+        self.picking_colors = False
+        self.swash_locations = []
+
+        # start watching the mouse
+        self.listener = mouse.Listener(on_click=self.mouse_click)
+
+    def draw_overlay_circles(self):
+        circles = []
+        for x, y in self.swash_locations:
+            circles.append(overlay.SkDrawCircle(overlay.Vector2D(x, y), 5, overlay.RgbaColor(255, 0, 0, 255), 5))
+
+        def callback():
+            return circles
+
+        tmp = overlay.Overlay(
+            drawlistCallback=callback,
+            refreshTimeout=500
+        )
+        tmp.spawn()
+
+    def mouse_click(self, x, y, button, pressed):
+        """function to run when the user clicks. used for both swashes and frames."""
+
+        if button == mouse.Button.left and pressed and self.picking_colors:  # if left click
+            if self.picking_colors:
+                self.swash_locations.append((x, y))  # add the position to the swashes
+                self.draw_overlay_circles()
 
     def color_cal_begin(self):
-        print("start color calibration!")
+        """begin color calibration"""
+        self.picking_colors = True
+
+        # start the mouse listener
+        if not self.listener.is_alive():
+            self.listener.start()
 
     def color_cal_finish(self):
-        print("finish color calibration!")
+        """end color calibration if the user has picked at least one swash
+        :returns: True if the user has picked at least one swash, None otherwise"""
+
+        # if there are swashes picked
+        if len(self.swash_locations) > 0:
+            self.picking_colors = False
+
+
+            # stop listening to the mouse
+            if self.listener.is_alive():
+                self.listener.stop()
+
+            return True
 
     def color_cal_reset(self):
+        """clear out the swash locations and restart picking"""
         print("reset color calibration!")
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from gui_fonts import *
 from calibrators import Calibrator
 
 # calibrator
-calibrator = Calibrator()
+c = Calibrator()
 
 # Window
 root = CTk()
@@ -20,17 +20,34 @@ frame_calibrate = make_frame(root)
 upload_img = make_frame(root)
 exit_page = make_frame(root)
 
-# Welcome page
-CTkLabel(welcomePage, text="Welcome to AutoPixelPaint!", font=title).pack()
-CTkButton(welcomePage, text="Begin", font=body, command=color_calibrate.tkraise).pack(side="bottom", pady=20)
 
-# Calibrate colors
+# Welcome page
+# ------------------------------------------------------------
+def color_start():
+    c.color_cal_begin()
+    color_calibrate.tkraise()
+
+
+CTkLabel(welcomePage, text="Welcome to AutoPixelPaint!", font=title).pack()
+CTkButton(welcomePage, text="Begin", font=body, command=color_start).pack(side="bottom", pady=20)
+
+
+# Calibrate colors page
+# ------------------------------------------------------------
+def color_finish():
+    result = c.color_cal_finish()
+    if result:
+        frame_calibrate.tkraise()
+    else:
+        print("pick more colors!")
+
+
 CTkLabel(color_calibrate, text="Click on each color swash in the \napplication that would like to use.", font=title).pack()
 
 c_cal_buttons_frame = CTkFrame(color_calibrate, fg_color="transparent")
 c_cal_buttons_frame.pack(pady=20)
 
-for button_text, cmd in (("Reset", calibrator.color_cal_reset), ("Finish", calibrator.color_cal_finish)):
+for button_text, cmd in (("Reset", c.color_cal_reset), ("Finish", color_finish)):
     CTkButton(c_cal_buttons_frame, text=button_text, command=cmd).pack(side="left", padx=10)
 
 # raise the welcome page to start
